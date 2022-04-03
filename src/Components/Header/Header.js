9029da4f38717294874a134d1b832fb232b6b426
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-04-04 00:20:03 +0600

    navlink addedt

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='nav-items'>
+                <Link to="/">Home</Link>
+                <Link to="/reviews">Reviews</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/about">About</Link>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='headline'>GET THE BEST VERSION OF YOUR LOOK STYLE</h1>
+            <img src="/" alt=""></img>
+        </div>
+       
+    );
+};
+
+export default Home;

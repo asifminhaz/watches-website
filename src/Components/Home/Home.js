9dec7be68fbfe455c0e4f8183e52bb1aae98fbf2
asifminhaz <minhazurrahmanasif40@gmail.com>
2022-04-04 01:06:04 +0600

    title and image added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,10 +3,17 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='headline'>GET THE BEST VERSION OF YOUR LOOK STYLE</h1>
-            <img src="/" alt=""></img>
-        </div>
+    <div className='products-info'>
+           <div className='headline'>
+              <h1 >GET THE BEST VERSION</h1>
+              <h1> OF YOUR LOOK STYLE</h1>
+              <p>A new look for your watch.your watch and strap should go together perfectly, but that doesn't mean you can't experiment with different combination. </p>
+          </div>
+          <div>
+              <img src="https://images.unsplash.com/photo-1622434641406-a158123450f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=404&q=80" alt=''></img>
+          </div>
+          <h1 className='reviews-item'>Customers Reviews</h1>
+     </div>
        
     );
 };
